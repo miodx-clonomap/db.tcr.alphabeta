d89f0ff75c1fa1414a84a747d15c13f9d8e31717
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2017-08-30 15:58:58 +0200

    add check for duplicates

--- src/test/scala/geneSequences.scala
+++ src/test/scala/geneSequences.scala
@@ -1,18 +1,34 @@
 package ohnosequences.db.tcr.test
 
 import ohnosequences.db.tcr._
+import ohnosequences.cosas._
+import ohnosequences.fastarious.fasta._
 
 class WellFormedInputs extends org.scalatest.FunSuite {
 
   val segments: Set[Segment] =
     Set(Segment.V, Segment.D, Segment.J)
 
-  test("human TCR beta") {
+  test("well-formed human TCR beta FASTA files") {
 
     segments foreach { segment =>
       inputData.sequences(Species.human, Chain.TRB, segment) foreach { lr =>
         assert( lr.isRight )
       }
+    }
+  }
+
+  test("TCR beta FASTA files have no duplicate IDs") {
+
+    segments foreach { segment =>
+
+      val ids =
+        inputData.sequences(Species.human, Chain.TRB, segment)
+          .collect { case Right(fa) => fa }
+          .map(fa => fa.getV(header).id)
+          .toSeq
+
+      assert { ids.distinct == ids }
     }
   }
 }
