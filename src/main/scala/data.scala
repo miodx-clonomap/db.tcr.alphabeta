69fc052625e2493bc1891586a855f4e51fbcdd73
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2017-08-30 11:12:50 +0200

    add S3 locations for data

---
+++ src/main/scala/data.scala
@@ -0,0 +1,39 @@
+package ohnosequences.db.tcr
+
+import ohnosequences.awstools.s3._
+
+case object data {
+
+  def base(
+    species : Species,
+    chain   : Chain,
+    segment : Segment
+  )
+  : S3Object =
+    s3prefix/species.taxonomyID/chain.name/segment.name
+
+  def objectPrefix(
+    species : Species,
+    chain   : Chain,
+    segment : Segment
+  )
+  : String =
+    s"${species.taxonomyID}.${chain.name}.${segment.name}"
+
+  def fasta(
+    species : Species,
+    chain   : Chain,
+    segment : Segment
+  )
+  : S3Object =
+    base(species, chain, segment) /
+      s"${objectPrefix(species, chain, segment)}.fasta"
+
+  def blastDB(
+    species : Species,
+    chain   : Chain,
+    segment : Segment
+  )
+  : S3Folder =
+    base(species, chain, segment)/"blast"/
+}
